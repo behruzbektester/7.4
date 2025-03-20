62fa2bac740339560c6509eafcf922d699ba4135
be <be@gmail.com>
2025-03-20 17:02:28 +0500

    modal ready

--- src/components/OrderConfirmed.jsx
+++ src/components/OrderConfirmed.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
-export default function OrderConfirmed() {
-  return <div>OrderConfirmed</div>;
+export default function OrderConfirmed({ onClose }) {
+  const { totalAmount } = useSelector((store) => store.cart);
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <div>
+          <img src="./images/icon-order-confirmed.svg" alt="Order Confirmed" />
+        </div>
+
+        <div className="modal-text">
+          <h2>Order Confirmed</h2>
+          <p>We hope you enjoy your food!</p>
+        </div>
+        <button
+          onClick={() => {
+            onClose();
+            totalAmount == 0;
+          }}
+          className="close-btn"
+        >
+          Start New Order
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/YourCart.jsx
+++ src/components/YourCart.jsx
@@ -1,22 +1,26 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import CartItem from "./CartItem";
+import OrderConfirmed from "./OrderConfirmed";
 
 export default function YourCart() {
   const { totalAmount, desserts, totalPrice } = useSelector(
     (store) => store.cart
   );
+  const [isOrderConfirmed, setIsOrderConfirmed] = useState(false);
+
   return (
     <div className="your-cart">
       <h2 className="your-cart-title">Your Cart ({totalAmount})</h2>
-      {desserts.map((d) => {
-        return d.amount !== 0 && <CartItem key={d.id} d={d} />;
-      })}
-      {totalAmount == 0 && (
+      {desserts.map((d) => d.amount !== 0 && <CartItem key={d.id} d={d} />)}
+
+      {totalAmount === 0 && (
         <div className="your-cart-empty">
-          <img src="./images/illustration-empty-cart.svg" alt="" />
+          <img src="./images/illustration-empty-cart.svg" alt="Empty Cart" />
           <p>Your added items will appear here</p>
         </div>
       )}
+
       {totalAmount !== 0 && (
         <div>
           <div className="order-total">
@@ -24,13 +28,22 @@ export default function YourCart() {
             <h2>${totalPrice}</h2>
           </div>
           <div className="delivery">
-            <img src="./images/icon-carbon-neutral.svg" alt="" />
+            <img src="./images/icon-carbon-neutral.svg" alt="Carbon Neutral" />
             <p>
               This is a <strong>carbon-neutral</strong> delivery
             </p>
           </div>
-          <button className="order-btn">Confirm Order</button>
+          <button
+            className="order-btn"
+            onClick={() => setIsOrderConfirmed(true)}
+          >
+            Confirm Order
+          </button>
         </div>
+      )}
+
+      {isOrderConfirmed && (
+        <OrderConfirmed onClose={() => setIsOrderConfirmed(false)} />
       )}
     </div>
   );
